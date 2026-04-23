40d83ef8eacc7205107b09cde0ea4e2713358146
Đỗ Đức Anh <anhdott1@gmail.com>
2026-04-23 18:56:53 +0700

    feat: Create friend item + friend page (#12)
    * feat: create friend item component
    
    * feat: create friend detail
    
    * feat: create friend page
    
    * style: add responsive breakpoint
    
    * feat: create friend item skeleton when loading data
    
    * fix: remove comment

---
+++ src/routes/learner/friends/-components/FriendItem/index.tsx
@@ -0,0 +1,50 @@
+import classNames from "classnames";
+import "./style.scss";
+import type { TFriendItem } from "./type";
+import FriendItemFooter from "./components/FriendFooter";
+
+type Props = TFriendItem & {
+  isActive?: boolean;
+};
+
+const FriendItem = (props: Props) => {
+  const { name, onClick, imgUrl, typeFriendItem, streaks, id, isActive } =
+    props;
+
+  return (
+    <button
+      className={classNames("friend-item", {
+        "friend-item--active": isActive,
+      })}
+      onClick={onClick}
+      tabIndex={0}
+    >
+      <div className="friend-item-index">
+        {id ? (
+          <span>{id}</span>
+        ) : (
+          <span className="friend-item-index-star">⭐</span>
+        )}
+      </div>
+
+      <div className="friend-item-info">
+        <img className="friend-item-info-img" src={imgUrl} alt={name} />
+        <p className="friend-item-info-name">{name}</p>
+      </div>
+      <FriendItemFooter
+        typeItem={typeFriendItem}
+        streaks={streaks}
+        onClickAccept={(e) => {
+          e.stopPropagation();
+          alert("Accept Friend");
+        }}
+        onClickDecline={(e) => {
+          e.stopPropagation();
+          alert("Decline User");
+        }}
+      />
+    </button>
+  );
+};
+
+export default FriendItem;
