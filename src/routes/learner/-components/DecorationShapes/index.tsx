83223008130fba908dc155d58e1012b4c4c5aa1b
huyhuyhuy8s <leminhhuy0404.work@gmail.com>
2026-04-23 18:30:20 +0700

    feat: home component

---
+++ src/routes/learner/-components/DecorationShapes/index.tsx
@@ -0,0 +1,71 @@
+import { COLORS } from "@/styles/colors";
+import {
+  Gem,
+  Pill,
+  SoftBurst,
+  Flower,
+  SidedCookie6,
+  Semicircle,
+  LeafClover4,
+} from "@/components/Shapes";
+import "./style.scss";
+
+const DecorationShapes = () => {
+  return (
+    <section className="decoration-shapes">
+      <div className="gem">
+        <span className="material-symbols-rounded">psychology</span>
+        <Gem size="320px" color={COLORS.modeOrange} />
+      </div>
+      <div className="pill">
+        <Pill
+          size="170px"
+          color={COLORS.modeNavy}
+          stroke={COLORS.modeYellow}
+          strokeWidth={50}
+        />
+      </div>
+      <div className="soft-burst">
+        <SoftBurst
+          size="240px"
+          color={COLORS.modeSalmon}
+          stroke={COLORS.modeGreen}
+          strokeWidth={45}
+        />
+      </div>
+      <div className="experiment">
+        <span className="material-symbols-rounded">experiment</span>
+      </div>
+      <div className="stylus_fountain_pen">
+        <span className="material-symbols-rounded">stylus_fountain_pen</span>
+      </div>
+      <div className="flower">
+        <Flower
+          size="300px"
+          color={COLORS.modeGreen}
+          stroke={COLORS.modeOrange}
+          strokeWidth={35}
+        />
+      </div>
+      <div className="school">
+        <span className="material-symbols-rounded">school</span>
+        <SidedCookie6 size="220px" color={COLORS.modeYellow} />
+      </div>
+      <div className="semicircle">
+        <Semicircle size="150px" color={COLORS.modeBrown} />
+        <Semicircle size="150px" color={COLORS.modeDarkGreen} />
+      </div>
+      <div className="clover">
+        <span className="material-symbols-rounded">school</span>
+        <LeafClover4
+          size="200px"
+          color={COLORS.modeYellow}
+          stroke={COLORS.modeGreen}
+          strokeWidth={40}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default DecorationShapes;
--- src/routes/learner/index.tsx
+++ src/routes/learner/index.tsx
@@ -1,24 +1,17 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
 import "./home.scss";
 import Search from "@/components/Search";
 import TextButton from "@/components/TextButton";
 import { SEARCH_SUGGESTIONS } from "@/mock/home";
-import { COLORS } from "@/styles/colors";
-import {
-  Gem,
-  Pill,
-  SoftBurst,
-  Flower,
-  SidedCookie6,
-  Semicircle,
-  LeafClover4,
-} from "@/components/Shapes";
+import DecorationShapes from "./-components/DecorationShapes";
 
 export const Route = createFileRoute("/learner/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const navigate = useNavigate();
+
   return (
     <div className="home">
       <div className="title">
@@ -42,64 +35,14 @@ function RouteComponent() {
               icon="subdirectory_arrow_right"
               roundedCorner="exceptUpperRight"
               size="medium"
-              onClick={() => {}}
+              onClick={() => {
+                navigate({ to: "/learner/search", search: { q: item } });
+              }}
             />
           ))}
         </div>
       </div>
-      <section className="decoration-shapes">
-        <div className="gem">
-          <span className="material-symbols-rounded">psychology</span>
-          <Gem size="320px" color={COLORS.modeOrange} />
-        </div>
-        <div className="pill">
-          <Pill
-            size="170px"
-            color={COLORS.modeNavy}
-            stroke={COLORS.modeYellow}
-            strokeWidth={50}
-          />
-        </div>
-        <div className="soft-burst">
-          <SoftBurst
-            size="240px"
-            color={COLORS.modeSalmon}
-            stroke={COLORS.modeGreen}
-            strokeWidth={45}
-          />
-        </div>
-        <div className="experiment">
-          <span className="material-symbols-rounded">experiment</span>
-        </div>
-        <div className="stylus_fountain_pen">
-          <span className="material-symbols-rounded">stylus_fountain_pen</span>
-        </div>
-        <div className="flower">
-          <Flower
-            size="300px"
-            color={COLORS.modeGreen}
-            stroke={COLORS.modeOrange}
-            strokeWidth={35}
-          />
-        </div>
-        <div className="school">
-          <span className="material-symbols-rounded">school</span>
-          <SidedCookie6 size="220px" color={COLORS.modeYellow} />
-        </div>
-        <div className="semicircle">
-          <Semicircle size="150px" color={COLORS.modeBrown} />
-          <Semicircle size="150px" color={COLORS.modeDarkGreen} />
-        </div>
-        <div className="clover">
-          <span className="material-symbols-rounded">school</span>
-          <LeafClover4
-            size="200px"
-            color={COLORS.modeYellow}
-            stroke={COLORS.modeGreen}
-            strokeWidth={40}
-          />
-        </div>
-      </section>
+      <DecorationShapes />
     </div>
   );
 }
