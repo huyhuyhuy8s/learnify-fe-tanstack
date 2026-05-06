c732f1c8c145e750e09fb00583f96be2588513d5
Đỗ Đức Anh <anhdott1@gmail.com>
2026-05-06 23:03:57 +0700

    feat: user profile

---
+++ src/hooks/useProfile.ts
@@ -0,0 +1,53 @@
+import { graphqlClient } from "@/lib/graphql";
+import { useQuery } from "@tanstack/react-query";
+import { GET_PROFILE } from "@/graphql/user";
+
+export type TBackendUser = {
+  id: string;
+  username: string;
+  email: string;
+  diamond: number;
+  currentSteak: number;
+  phoneNumber: string;
+  role: string;
+};
+
+export type TProfileCourse = {
+  id: string;
+  courseName: string;
+  abstract: string;
+  isDone: boolean;
+  keyLearnings: string[];
+};
+
+export type GetUserProfileResponse = {
+  currentUser: {
+    count: number;
+    isSuccess: boolean;
+    message: string;
+    users: TBackendUser[];
+  };
+  countSuccessEnrollments: {
+    count: number;
+    data: TProfileCourse[];
+  };
+  countInProgressEnrollments: {
+    count: number;
+    data: TProfileCourse[];
+    progress: number;
+  };
+};
+
+export function useGetUserProfile(userId: string) {
+  return useQuery({
+    queryKey: ["user", "profile", userId],
+    queryFn: async () => {
+      const response = await graphqlClient.request<GetUserProfileResponse>(
+        GET_PROFILE,
+        { userId }
+      );
+      return response;
+    },
+    enabled: !!userId,
+  });
+}
