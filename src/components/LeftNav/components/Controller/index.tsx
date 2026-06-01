894ff8ef2ca12f43183e9d8fc29f2bec21d09b33
huyhuyhuy8s <leminhhuy0404.work@gmail.com>
2026-06-01 15:40:59 +0700

    style: update responsiveness for left nav

--- src/components/LeftNav/components/Controller/index.tsx
+++ src/components/LeftNav/components/Controller/index.tsx
@@ -13,7 +13,7 @@ const Controller = (props: TControllerProps) => {
 
   return (
     <button
-      className={classnames("left-nav-controller", { active }, className)}
+      className={classnames("left-nav-controller", className)}
       onClick={onClick}
       type="button"
       aria-label={
